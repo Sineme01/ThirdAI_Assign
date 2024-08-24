b69e9fe20757094de04801d1e93ea8dbfb009ae7
Anand Kumar <anandkr7658@gmail.com>
2024-08-24 10:36:26 +0530

    App Layout, Routing and Tailwind CSS config
    In this commit firstly I structured all the directory and created all the necessary components.
    Then Routing was done with the help of React Router Dom. I then configured Tailwind CSS.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import logo from "./assets/logo192.png";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Form from "./components/form.js";
+import Read from "./components/read.js";
+import Header from './components/header.js';
+import Home from "./components/home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="flex flex-col min-h-screen">
+      <BrowserRouter>
+        <Header />
+        <div className="flex-1">
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/form' element={<Form />}></Route>
+            <Route path='/read' element={<Read />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
