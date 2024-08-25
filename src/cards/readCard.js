af69a831722c30dc966abd5719076d9cc388aad3
Anand Kumar <anandkr7658@gmail.com>
2024-08-25 23:18:23 +0530

    made the read card adaptable according to screen size

--- src/cards/readCard.js
+++ src/cards/readCard.js
@@ -1,7 +1,10 @@
 const ReadCard = ({ id, title, authorships }) => {
     return (
-        <div key={id} className="bg-gray-100 p-4 mb-4 rounded-md shadow-md mx-52">
-            <div className="px-8">
+        <div
+            key={id}
+            className="bg-gray-100 p-4 mb-4 rounded-md shadow-md mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-52"
+        >
+            <div className="px-2 sm:px-4 md:px-6 lg:px-8">
                 <h3 className="text-lg font-semibold mb-2">{title}</h3>
                 <p className="text-sm text-gray-700">
                     {authorships && authorships.map((author) => author.author.display_name).join(', ')}
